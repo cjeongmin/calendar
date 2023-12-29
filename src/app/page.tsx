cabf16ddf58e970be863ea4a205c43ab8dca6a2c
cjeongmin <cjeongmin27@gmail.com>
2023-12-30 04:48:54 +0900

    feat: add nav utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import styles from "@/styles/page.module.scss";
+import styles from "@/styles/app-page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.utils}>
+        <div className={styles["add-button"]}>+</div>
+        <div className={styles.types}>
+          <span>일</span>
+          <span>주</span>
+          <span>월</span>
+          <span>년</span>
+        </div>
+        <input placeholder={"검색"} className={styles.search} />
+      </div>
+    </main>
+  );
 }
--- src/components/RecoilProvider.tsx
+++ src/components/RecoilProvider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { RecoilRoot } from "recoil";
 
 export default function RecoilProvider({
