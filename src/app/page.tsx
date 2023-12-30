d094a837812f131f751b9b10cb92b7fa791a4c9a
cjeongmin <cjeongmin27@gmail.com>
2023-12-30 11:06:47 +0900

    feat: add main header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import styles from "@/styles/app-page.module.scss";
 
 export default function Home() {
+  const today = new Date();
+  const [year, month] = [today.getFullYear(), today.getMonth() + 1];
+
   return (
     <main className={styles.main}>
       <div className={styles.utils}>
@@ -13,6 +16,20 @@ export default function Home() {
         </div>
         <input placeholder={"검색"} className={styles.search} />
       </div>
+      <header className={styles.header}>
+        <h1>{`${year}년 ${month}월`}</h1>
+        <div className={styles.buttons}>
+          <button>
+            <span className="material-symbols-outlined">arrow_back_ios</span>
+          </button>
+          <button>
+            <div>오늘</div>
+          </button>
+          <button>
+            <span className="material-symbols-outlined">arrow_forward_ios</span>
+          </button>
+        </div>
+      </header>
     </main>
   );
 }
