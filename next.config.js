74516d400820c50372845d52cd5dc8973f9e43a3
cjeongmin <cjeongmin27@gmail.com>
2024-01-01 18:55:20 +0900

    chore: change files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/calendar/` : undefined,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
