b30bf8c38ff56f1b3f8316617486a1c490ca270a
cjeongmin <cjeongmin27@gmail.com>
2024-01-01 18:45:06 +0900

    chore: change next.config, package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://raw.githubusercontent.com/cjeongmin/calendar/gh-pages/"
-      : undefined,
 };
 
 module.exports = nextConfig;
