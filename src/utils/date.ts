eba02e0aeccb8e1411795f7a8b3452c07b2a240b
cjeongmin <cjeongmin27@gmail.com>
2024-01-08 15:15:37 +0900

    feat: (#3) add schedules hooks

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -42,3 +42,19 @@ export const isSameDate: (date1: Date, date2: Date) => boolean = (
   }
   return false;
 };
+
+export const isEqual: (date1: Date, date2: Date) => boolean = (
+  date1,
+  date2
+) => {
+  if (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate() &&
+    date1.getHours() === date2.getHours() &&
+    date1.getMinutes() === date2.getMinutes()
+  ) {
+    return true;
+  }
+  return false;
+};
